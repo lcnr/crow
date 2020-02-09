e459ff3fa079b835727f4f7d709abae9b3ef24cf
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-02-09 11:48:28 +0100

    add benchmark

---
+++ benches/debug_lines.rs
@@ -0,0 +1,39 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use crow::{
+    glutin::{EventsLoop, WindowBuilder},
+    Context,
+};
+
+fn debug_lines(c: &mut Criterion) {
+    let mut ctx = Context::new(
+        WindowBuilder::new()
+            .with_dimensions(From::from((720, 480)))
+            .with_visibility(false),
+        EventsLoop::new(),
+    )
+    .unwrap();
+
+    let mut surface = ctx.window_surface();
+
+    c.bench_function("debug_lines", |b| {
+        b.iter(|| {
+            ctx.clear_color(&mut surface, (0.0, 0.0, 0.0, 1.0)).unwrap();
+
+            for i in (0..100).map(|i| i * 2) {
+                ctx.draw_line(
+                    &mut surface,
+                    (i, i + 10),
+                    (i + 20, i + 10),
+                    (1.0, 0.0, 1.0, 1.0),
+                )
+                .unwrap();
+            }
+
+            ctx.finalize_frame().unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, debug_lines);
+criterion_main!(benches);
