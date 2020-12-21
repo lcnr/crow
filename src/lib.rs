6133d896d3dfc24e62f3162c25ff8d9056490a30
Vincent Higginson <vincent.higginson@student.unamur.be>
2020-12-21 19:06:56 +0100

    Update src/lib.rs according to maintainer review.
    Co-authored-by: lcnr <bastian_kauschke@hotmail.de>

--- src/lib.rs
+++ src/lib.rs
@@ -318,6 +318,9 @@ pub struct DrawConfig {
     /// The scale of the drawn texture in drawn pixels per source pixel.
     pub scale: (u32, u32),
     /// The rotation of the drawn texture in degrees.
+    ///
+    /// Rotating by anything more precise than 90 degree steps can cause the source image
+    /// to be slightly distorted, especially if it has a very low resolution.
     pub rotation: i32,
     /// If the texture should be flipped on the y axis.
     pub flip_vertically: bool,
